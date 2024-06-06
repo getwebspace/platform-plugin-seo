e12ca1ae776ef7526cefb09b96a0a2a46664b66f
Aleksey Ilyin <alksily@outlook.com>
2024-06-06 15:34:07 +0300

    version+

--- SearchOptimization/SearchOptimizationPlugin.php
+++ SearchOptimization/SearchOptimizationPlugin.php
@@ -22,7 +22,7 @@ class SearchOptimizationPlugin extends AbstractPlugin
                         '<a href="/robots.txt" target="_blank">robots.txt</a>';
     const AUTHOR = 'Aleksey Ilyin';
     const AUTHOR_SITE = 'https://getwebspace.org';
-    const VERSION = '7.0.0';
+    const VERSION = '8.0.0';
 
     public function __construct(ContainerInterface $container)
     {
